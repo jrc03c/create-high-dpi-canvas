8aacbf754d3d00889664187d856c137aeebfc8e9
jrc03c <jrc03c@pm.me>
2021-04-19 20:22:00 -0500

    ...

---
+++ index.js
@@ -0,0 +1,18 @@
+function createHighDPICanvas(width, height){
+  width = parseInt(width)
+  height = parseInt(height)
+
+  let dpi = window.devicePixelRatio || 1
+  let canvas = document.createElement("canvas")
+  canvas.width = width * dpi
+  canvas.height = height * dpi
+  canvas.style.width = `${width}px`
+  canvas.style.height = `${height}px`
+
+  let context = canvas.getContext("2d")
+  context.scale(dpi, dpi)
+
+  return canvas
+}
+
+module.exports = createHighDPICanvas
